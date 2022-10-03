6b1ef0f0b29210dcce95a37760c998217b8155f6
rahul <rahul@LAPTOP-T6G63HRD>
2022-10-03 18:26:13 +0530

    Add project files.

---
+++ CustomKeyword/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemos
+{
+    // custom / user defined exception
+    public class AgeException : Exception
+    {
+        public AgeException(string msg) : base(msg)
+        {
+        }
+
+    }
+    public class User
+    {
+        public void AcceptAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new AgeException("Your age is not valid for vote, ");
+            }
+            else
+            {
+                throw new AgeException("Your age is valid for vote, ");
+            }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                user.AcceptAge(age);
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+}
+
+
